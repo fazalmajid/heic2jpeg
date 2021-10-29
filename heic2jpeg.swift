7af7b7e335e840aed994a22b18a0b7f03fe51ee5
Fazal Majid <majid@singular.net>
2021-10-29 14:19:44 +0100

    added option to delete the original HEIC file

--- heic2jpeg.swift
+++ heic2jpeg.swift
@@ -6,10 +6,15 @@ let context = CIContext(options: nil)
 let options = NSDictionary(
     dictionary: [kCGImageDestinationLossyCompressionQuality:jpegQuality]
 )
-
+var delete:Bool
 var filename:String?
+delete = false
 for i in 1..<Int(CommandLine.argc) {
     filename = CommandLine.arguments[i]
+    if filename == "-delete" || filename == "--delete" {
+        delete = true
+        continue
+    }
     let srcURL = URL(fileURLWithPath:filename!)
     let destURL = srcURL.deletingPathExtension().appendingPathExtension("jpg")
     var exists:Bool
@@ -23,10 +28,18 @@ for i in 1..<Int(CommandLine.argc) {
     } else {
         print("converting \(filename ?? "???")")
         let image = CIImage(contentsOf: srcURL)
-        try! context.writeJPEGRepresentation(
-            of:image!,
-            to:destURL,
-            colorSpace: image!.colorSpace!,
-            options:options as! [CIImageRepresentationOption : Any])
+        do {
+            try context.writeJPEGRepresentation(
+                of:image!,
+                to:destURL,
+                colorSpace: image!.colorSpace!,
+                options:options as! [CIImageRepresentationOption : Any]
+            )
+            if delete {
+                print("deleting \(filename ?? "???")")
+                try FileManager.default.removeItem(at:srcURL)
+            }
+        } catch {
+        }
     }
 }
